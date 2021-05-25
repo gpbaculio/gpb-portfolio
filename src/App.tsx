26a4dfacf1d13e8a7632086fd6871128d9cfa421
gpbaculio <gpbaculio@gmail.com>
2021-05-26 02:31:23 +0800

    add mousemove listener

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { useEffect, useRef } from "react";
 import styled from "styled-components";
 
 import "./App.css";
-
+type CanvasMouseType = {
+  x: number | null;
+  y: number | null;
+  radius: number;
+};
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -12,6 +16,16 @@ function App() {
       const canvasCtx = canvas.getContext("2d");
       canvas.width = window.innerWidth;
       canvas.height = window.innerHeight;
+      let particles;
+      let mouse: CanvasMouseType = {
+        x: null,
+        y: null,
+        radius: (canvas.height / 80) * (canvas.width / 80),
+      };
+      window.addEventListener("mousemove", function (event) {
+        mouse.x = event.x;
+        mouse.y = event.y;
+      });
     }
   }, []);
 
