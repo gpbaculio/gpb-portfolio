0534b93a786cfb0fa5437d2f72b941258126b228
gpbaculio <gpbaculio@gmail.com>
2021-05-26 03:22:25 +0800

    add update method on class

--- src/App.tsx
+++ src/App.tsx
@@ -60,12 +60,40 @@ function App() {
 
         //check particle position, check mouse position, move the particle, draw the particle
         update() {
-          //check if particle is still within canvas, if it has, reverse direction x, y values in opposite direction
-          if (this.x > canvas.width || this.x < 0) {
-            this.directionX = -this.directionX;
-          }
-          if (this.y > canvas.height || this.y < 0) {
-            this.directionY = this.directionY;
+          if (mouse && mouse.x && mouse.y) {
+            //check if particle is still within canvas, if it has, reverse direction x, y values in opposite direction
+            if (this.x > canvas.width || this.x < 0) {
+              this.directionX = -this.directionX;
+            }
+            if (this.y > canvas.height || this.y < 0) {
+              this.directionY = this.directionY;
+            }
+
+            // check collision detection - mouse position / particle position
+            let dx = mouse.x - this.x;
+            let dy = mouse.y - this.y;
+            let distance = Math.sqrt(dx * dx + dy * dy);
+            if (distance < mouse.radius + this.size) {
+              if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
+                this.x += 10;
+              }
+              if (mouse.x > this.x && this.x > this.size * 10) {
+                this.x -= 10;
+              }
+              if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
+                this.y += 10;
+              }
+              if (mouse.y > this.y && this.y > this.size * 10) {
+                this.y -= 10;
+              }
+            }
+
+            // move particle
+            this.x += this.directionX;
+            this.y += this.directionY;
+
+            // draw particle
+            this.draw();
           }
         }
       }
